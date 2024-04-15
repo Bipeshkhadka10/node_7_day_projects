e0f919ca9dd29ff1e9bbac4edfe881a18ec36b9b
Bipeshkhadka <bipeshkhadka2003@gmail.com>
2024-04-15 23:38:40 +0545

    nodejs project base for developing

---
+++ app.js
@@ -0,0 +1,9 @@
+// can be connected to database and server side rendring use for machine learning or for ai
+const express = require('express');
+const app = express()
+
+
+
+app.listen(3000,()=>{
+    console.log("server is started at port 3000")
+})
