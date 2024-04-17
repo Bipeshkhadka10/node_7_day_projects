8de645cd7e2454063181a8ca541772d8ceebeacc
Bipeshkhadka <bipeshkhadka2003@gmail.com>
2024-04-17 23:02:52 +0545

    learn about nodemon,get/request and response cycle,ejs

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ const express = require('express');
 const app = express()
 
 
+app.get('/',(req,res)=>{
+    res.render('home.ejs')        // we can also qrite html inside of res.send as
+})
+app.get('/blog',(req,res)=>{
+    const data={
+        name:'bipesh',
+        age:20,
+        gender:'male'
+    }
+    res.render('blog.ejs',{data})
+})
+app.get('/blog/create',(req,res)=>{
+    res.render('create.ejs')
+})
+app.get('/blog/edit',(req,res)=>{
+    res.render('edit.ejs')
+})
 
 app.listen(3000,()=>{
-    console.log("server is started at port 3000")
+    console.log("This is nodejs project")
 })
